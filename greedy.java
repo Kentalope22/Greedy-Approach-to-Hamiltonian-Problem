2d34792caafae6ab61175ee0f22cd2d1c390d837
Kentalope22 <69648019+Kentalope22@users.noreply.github.com>
2025-02-23 16:30:26 -0800

    created test arrays & vars

--- greedy.java
+++ greedy.java
@@ -1,1 +1,10 @@
-
+public class greedy{
+    int[] city_distances = {5,25,15,10,15};
+    int[] fuel = {1,2,1,0,3};
+    int mpg = 10;
+    public static void main(String[] args) {
+        
+    }
+    
+    
+}
